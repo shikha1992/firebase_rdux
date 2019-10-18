394f23b1e5bc12692eaa07765e51c818863f37d3
mohit <Mohitkau1010@gmail.com>
2019-10-19 04:33:00 +0530

    without redux

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/pages/root_page.dart';
+import 'package:flutter_firebase/services/authentication.dart';
+
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
